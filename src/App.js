22a6aee8d5b40c9eb90c13aa8a7c5f3285f65a25
NICOLLAS CANE SEULA <nicollas_seula@estudante.sesisenai.org.br>
2026-06-22 17:08:27 -0300

    Base do projeto

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Login from './components/Login';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Login />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Login/index.js
@@ -0,0 +1,11 @@
+import styles from './Login.module.css';
+
+function Login () {
+    return (
+        <login className={styles.login}>
+            <h2>Login</h2>
+        </login>
+    )
+}
+
+export default Login;
