a11702f4f5580201d8460f891ae61c52c0a37657
NICOLLAS CANE SEULA <nicollas_seula@estudante.sesisenai.org.br>
2026-07-01 16:31:48 -0300

    Tentei fazer algo no Login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,14 @@
 import styles from './Login.module.css';
 
-function Login () {
-    return (
-        <login className={styles.login}>
-            <h2>Login</h2>
-        </login>
-    )
+function Login() {
+  return (
+    <div className={styles.login}>
+      <h2>Login</h2>
+      <input type="text" placeholder="Nome" />
+      <input type="email" placeholder="Email" />
+      <button>Enviar</button>
+    </div>
+  );
 }
 
 export default Login;
