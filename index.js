57369497ffa4f10400adc54ca4c5f81559e77956
GABRIEL HENRIQUE SALM DE BORBA <gabriel_hs_borba@estudante.sesisenai.org.br>
2026-07-06 17:07:02 -0300

    aaabbcc

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
 require('dotenv').config();
 
-const express = require('express');
-const routes = require('./src/config/routes');
 const database = require('./src/config/database');
+const express = require('express');
+const Routes = require('./src/config/routes');
 
-const app = express();
+database.authenticate();
+
+let app = express();
 app.use(express.json());
+Routes(app);
 
-routes(app);
-
-async function startServer() {
-  try {
-    await database.authenticate();
-    console.log('Conexão com o banco de dados estabelecida.');
-
-    await database.sync();
-    console.log('Banco de dados sincronizado.');
-
-    app.listen(3000, () => {
-      console.log('Server is running on http://127.0.0.1:3000');
-    });
-  } catch (error) {
-    console.error('Erro ao iniciar o servidor:', error);
-    process.exit(1);
-  }
-}
-
-startServer();
+database.sync({ alter: true });
+app.listen(3000);
